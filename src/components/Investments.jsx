b67d97a99d56503fcbe724c4b682bfa2e95134fd
Samuel Cardenas <samcard1999@gmail.com>
2025-09-01 13:53:31 -0400

    hola

--- src/components/Investments.jsx
+++ src/components/Investments.jsx
@@ -7,11 +7,11 @@ const Investments = () => {
   return (
     <section
       id="investments"
-      className="w-full h-screen flex flex-col justify-between bg-primary z-20 gap-12 py-32 px-8 items-center relative"
+      className="w-full h-screen flex flex-col justify-center bg-primary z-20 gap-12 py-32 px-8 items-center relative"
     >
       <div className="absolute inset-0 -z-10 opacity-80 bg-black"></div>
       <AnimatedBackground />
-      <div className="flex flex-col w-full gap-4 justify-start items-start">
+      <div className="flex flex-col w-full gap-4 justify-start items-start 2xl-custom:absolute 2xl-custom:left-0 2xl-custom:top-32 2xl-custom:px-8 2xl:relative 2xl:inset-auto 2xl:px-0">
         <div className="flex w-full justify-between items-center">
           <h1 className="lg:text-5xl text-[2.1rem] lg:leading-normal leading-10 font-bold">
             Why invest with DDC Developments?
@@ -29,7 +29,7 @@ const Investments = () => {
         </PrimaryButton>
       </div>
 
-      <div className="flex flex-col w-full lg:gap-12 max-w-[90%] lg:max-w-[70%] h-full">
+      <div className="flex flex-col w-full lg:gap-12 max-w-[90%] lg:max-w-[70%] h-auto">
         <div className="lg:flex-row flex flex-col">
           <div className="w-full h-full flex flex-col justify-center items-center relative py-6 lg:py-0">
             <span className="absolute -top-5 left-0 -translate-x-1/2 lg:-translate-x-1/2 text-primary opacity-40 text-2xl lg:text-4xl">
