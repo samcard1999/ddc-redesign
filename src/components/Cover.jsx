f805596e4142247a88537727907ee528a1d14334
Samuel Cardenas <samcard1999@gmail.com>
2025-09-05 20:10:35 -0400

    Seccion Our Process

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -106,7 +106,7 @@ const Cover = () => {
 
         <h1
           ref={buildRef}
-          className="absolute top-0 left-0 pointer-events-none select-none will-change-transform
+          className="absolute -top-2 lg:-top-4 left-0 pointer-events-none select-none will-change-transform
                      text-transparent font-extrabold text-3xl lg:text-8xl"
           style={{ WebkitTextStroke: "1px #c2c7cf" }}
         >
